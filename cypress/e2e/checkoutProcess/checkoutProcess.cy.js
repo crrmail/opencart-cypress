8f705202591509501ab07b615df58c42cf46d30b
crrmail <preeyamon.tester@gmail.com>
2024-10-28 11:11:05 +0700

    update tc-checkout process

--- cypress/e2e/checkoutProcess/checkoutProcess.cy.js
+++ cypress/e2e/checkoutProcess/checkoutProcess.cy.js
@@ -1,14 +1,16 @@
 import testData from '../loginData.json'
+import { checkoutWithGuestUser,popupMessageErrorAlertInvalidEmailPassword,personalInfo2,personalInfo3,submitFormwithUncheck, } from './util'
 
-describe('Checkout process',() => {
+describe('Checkout process', () => {
     beforeEach(() => {
         // home page
         cy.visit('https://opencart.abstracta.us/index.php?route=common/home')
     })
-    it('tc021 : Checkout after login successful',() => {
+
+    it('tc021 : Checkout after login successful', () => {
         // login success
         cy.goToLoginFromHome()
-        cy.loginWithEmailandPassword(testData.validEmail,testData.validPassword)
+        cy.loginWithEmailandPassword(testData.validEmail, testData.validPassword)
         cy.get('h1 > a').click()
 
         // add item to cart
@@ -18,7 +20,8 @@ describe('Checkout process',() => {
         cy.get(':nth-child(4) > a > .fa').click()
         cy.get('.pull-right > .btn').click()
     })
-    it('tc022 : Checkout without Login with (Guest )',() => {
+
+    it('tc022 : Checkout without Login with (Guest)', () => {
         // add item to cart
         cy.addItemToCart()
 
@@ -30,12 +33,13 @@ describe('Checkout process',() => {
         cy.get(':nth-child(4) > label > input').click()
         cy.get('#button-account').click()
     })
-    it('tc023 : Submit Form with all required data',() => {
+
+    it('tc023 : Submit Form with all required data', () => {
         // add and checkout item
-        cy.tc022()
+        checkoutWithGuestUser()
 
         // Personal info
-        cy.personalInfo2()
+        personalInfo2()
 
         // Submit Form
         cy.get('#button-guest').click()
@@ -43,9 +47,10 @@ describe('Checkout process',() => {
         //
         cy.get('#collapse-shipping-method > .panel-body').should('be.visible')
     })
-    it('tc024 : Submit Form with missing required data',() => {
+
+    it('tc024 : Submit Form with missing required data', () => {
         // add and checkout item
-        cy.tc022()
+        checkoutWithGuestUser()
 
         // Submit Form
         cy.get('#button-guest').click()
@@ -53,12 +58,13 @@ describe('Checkout process',() => {
         // verify 
         cy.get('#account > :nth-child(3) > .text-danger').should('be.visible')
     })
-    it.only('tc025 : Submit Form with all required data (Uncheck “My delivery and billing addresses are the same”)',() => {
+
+    it.only('tc025 : Submit Form with all required data (Uncheck “My delivery and billing addresses are the same”)', () => {
         // add and checkout item
-        cy.tc022()
+        checkoutWithGuestUser()
 
         // Personal info step 2
-        cy.personalInfo2()
+        personalInfo2()
 
         // Uncheck “My delivery and billing addresses are the same”
         cy.get('.checkbox > label > input').click()
@@ -69,13 +75,16 @@ describe('Checkout process',() => {
         // verify
         cy.get('#collapse-shipping-address > .panel-body').should('be.visible')
     })
-    it.only('tc026 : Submit Form with all required information',() => {
-        cy.tc025()
+
+    it.only('tc026 : Submit Form with all required information', () => {
+        // tc 025
+        submitFormwithUncheck()
 
         // Personal info step 3
-        cy.personalInfo3()
+        personalInfo3()
 
+        
         // submit
-        cy.get('#button-guest-shipping').click()
+        //cy.get('#button-guest-shipping').click()
     })
 })
---
+++ cypress/e2e/checkoutProcess/util.js
@@ -0,0 +1,65 @@
+const checkoutWithGuestUser = () => {
+    // add item to cart
+    cy.addItemToCart()
+
+    // checkout
+    cy.get(':nth-child(4) > a > .fa').click()
+    cy.get('.pull-right > .btn').click()
+
+    // select Guest Checkout
+    cy.get(':nth-child(4) > label > input').click()
+    cy.get('#button-account').click()
+}
+
+const popupMessageErrorAlertInvalidEmailPassword = () => {
+    cy.get('.alert').should('be.visible')
+    cy.get('.alert').should('have.text',' Warning: No match for E-Mail Address and/or Password.')
+}
+
+const personalInfo2 = () => {
+    cy.get('#input-payment-firstname').type('David')
+    cy.get('#input-payment-lastname').type('Roger')
+    cy.get('#input-payment-email').type('david@email.com')
+    cy.get('#input-payment-telephone').type('0912223333')
+    cy.get('#input-payment-address-1').type('199 Bangna Tai')
+    cy.get('#input-payment-city').type('Bangna')
+    cy.get('#input-payment-postcode').type('10900')
+    cy.get('#input-payment-country').select('Thailand')
+    cy.get('#input-payment-zone').select('Bangkok')
+}
+
+const personalInfo3 = () => {
+    cy.get('#input-shipping-firstname').type('David')
+    cy.get('#input-shipping-lastname').type('Roger')
+    cy.get('#input-shipping-address-1').type('199 Bangna Tai')
+    cy.get('#input-shipping-city').type('Bangna')
+    cy.get('#input-shipping-postcode').type('10900')
+    cy.get('#input-shipping-country').select('Thailand')
+    cy.get('#input-shipping-zone').type('Bangkok')
+}
+
+
+const submitFormwithUncheck = () => {
+    // add and checkout item
+    checkoutWithGuestUser()
+
+    // Personal info step 2
+    cy.personalInfo2()
+
+    // Uncheck “My delivery and billing addresses are the same”
+    cy.get('.checkbox > label > input').click()
+
+    // Submit Form
+    cy.get('#button-guest').click()
+}
+
+
+
+export default {
+    checkoutWithGuestUser,
+    popupMessageErrorAlertInvalidEmailPassword,
+    personalInfo2,
+    submitFormwithUncheck,
+    personalInfo3
+}
+
